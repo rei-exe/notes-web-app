cf7ded9fd4f1ed257b3ba96a22e9d1fe9d5b7888
Jamesnl55555 <jamesnl55555@gmail.com>
2024-10-07 15:54:01 +0800

    nagka problema lng, bnalik ko lng

--- routes/web.php
+++ routes/web.php
@@ -2,22 +2,17 @@
 
 use App\Http\Controllers\NoteController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\showAllNotes;
 
 Route::get('/', function () {
     return view('welcome');
 });
-
-// // <<<<<<< HEAD
-// Route::get('/home', function(){
-//     return view('home');
-// });
-// //=======
-Route::get('/notes', [NoteController::class, 'ShowAllNotesController']); 
-Route::get('/notes/create', [NoteController::class, 'CreateNotesController']);
-Route::post('/notes', [NoteController::class, 'StoreNotesController']); 
-Route::get('/notes/{id}', [NoteController::class, 'ShowANoteController']);
-Route::get('/notes/{id}/edit', [NoteController::class, 'EditNoteController']);
-Route::put('/notes/{id}', [NoteController::class, 'UpdateNoteController']); 
-Route::delete('/notes/{id}', [NoteController::class, 'DestroyNoteController']);
-// >>>>>>> ec7d87da0e4386d49002693765bc1304034b419d
+Route::get('/home', function(){
+    return view('home');
+});
+Route::get('/notes', [NoteController::class, 'showAllNotes']); 
+Route::get('/notes/create', [NoteController::class, 'create']);
+Route::post('/notes', [NoteController::class, 'store']); 
+Route::get('/notes/{id}', [NoteController::class, 'show']);
+Route::get('/notes/{id}/edit', [NoteController::class, 'edit']);
+Route::put('/notes/{id}', [NoteController::class, 'update']); 
+Route::delete('/notes/{id}', [NoteController::class, 'destroy']);
