2f1444743e645a15f5cf24135b7a124e65ca132d
Jamesnl55555 <jamesnl55555@gmail.com>
2024-10-07 14:29:37 +0800

    commented all route

--- routes/web.php
+++ routes/web.php
@@ -8,16 +8,16 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// <<<<<<< HEAD
-Route::get('/home', function(){
-    return view('home');
-});
-//=======
-Route::get('/notes', [NoteController::class, 'showAllNotes']); 
-Route::get('/notes/create', [NoteController::class, 'create']);
-Route::post('/notes', [NoteController::class, 'store']); 
-Route::get('/notes/{id}', [NoteController::class, 'show']);
-Route::get('/notes/{id}/edit', [NoteController::class, 'edit']);
-Route::put('/notes/{id}', [NoteController::class, 'update']); 
-Route::delete('/notes/{id}', [NoteController::class, 'destroy']);
-// >>>>>>> ec7d87da0e4386d49002693765bc1304034b419d
+// // <<<<<<< HEAD
+// Route::get('/home', function(){
+//     return view('home');
+// });
+// //=======
+// Route::get('/notes', [NoteController::class, 'ShowAllNotes']); 
+// Route::get('/notes/create', [NoteController::class, 'Create']);
+// Route::post('/notes', [NoteController::class, 'Store']); 
+// Route::get('/notes/{id}', [NoteController::class, 'Show']);
+// Route::get('/notes/{id}/edit', [NoteController::class, 'Edit']);
+// Route::put('/notes/{id}', [NoteController::class, 'Update']); 
+// Route::delete('/notes/{id}', [NoteController::class, 'Destroy']);
+// // >>>>>>> ec7d87da0e4386d49002693765bc1304034b419d
