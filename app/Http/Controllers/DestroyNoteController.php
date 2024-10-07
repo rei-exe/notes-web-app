ca24cb893a324ca68e5ea3585d5935220b0499af
Jamesnl55555 <jamesnl55555@gmail.com>
2024-10-07 15:08:09 +0800

    same, modified

--- app/Http/Controllers/DestroyNoteController.php
+++ app/Http/Controllers/DestroyNoteController.php
@@ -2,10 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Models\Note;
 
 class DestroyNoteController extends Controller
 {
-    //
+    public function destroy($id)
+    {
+        $note = Note::findOrFail($id); # fetch note
+        $note->delete(); # delete note
+        return redirect('/notes'); # redirects to landing page
+    }
 }
