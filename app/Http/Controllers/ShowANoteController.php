c972305a47f9c0271c1dc99bd5b4b149a0b5dc0f
Jamesnl55555 <jamesnl55555@gmail.com>
2024-10-07 15:00:14 +0800

    same, modified

--- app/Http/Controllers/ShowANoteController.php
+++ app/Http/Controllers/ShowANoteController.php
@@ -2,10 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
+use App\Models\Note;
 use Illuminate\Http\Request;
 
 class ShowANoteController extends Controller
 {
-    //
+    public function show($id)
+    {
+        $note = Note::findOrFail($id); # fetch notes
+        return view('view', compact('note')); # return a note
+    }
 }
