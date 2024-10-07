2fdf0f3017f5a0a9c905f2efb72a0b0f9b0364ba
Jamesnl55555 <jamesnl55555@gmail.com>
2024-10-07 14:08:07 +0800

    modified:   app/Http/Controllers/showAllNotes.php

--- app/Http/Controllers/showAllNotes.php
+++ app/Http/Controllers/showAllNotes.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class showAllNotes extends Controller
+class ShowAllNotes extends Controller
 {
     //
 }
