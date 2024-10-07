fa55837b65bf8f89df1acc94d603a839bd915f08
Jamesnl55555 <jamesnl55555@gmail.com>
2024-10-07 14:58:04 +0800

    same, modified

--- app/Http/Controllers/StoreNotesController.php
+++ app/Http/Controllers/StoreNotesController.php
@@ -2,10 +2,22 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
+use App\Models\Note;
 use Illuminate\Http\Request;
 
 class StoreNotesController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        # validation
+        $request->validate([
+            'title' => 'required|max:100',
+            'description' => 'nullable|max:500',
+            'content' => 'required|max:10000',
+        ]);
+
+        Note::create($request->all()); # validated data becomes notes
+
+        return redirect('/notes'); # redirects to landing page
+    }
 }
