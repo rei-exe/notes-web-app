ebedcaa4d15bd179bd98daf6acfb59cbc8afd047
Jamesnl55555 <jamesnl55555@gmail.com>
2024-10-07 16:36:57 +0800

    added file

---
+++ resources/views/about.blade.php
@@ -0,0 +1,3 @@
+<div>
+    <h1>This is a note app, dont complain</h1>
+</div>
---
+++ resources/views/contributors.blade.php
@@ -0,0 +1,8 @@
+<div>
+    <h1>Contributors:</h1>
+    <ul>
+        <li type="square">Vincent Duriga</li>
+        <li type="square">John Russel Regalado</li>
+        <li type="square">Jimmy N. Lumaad</li>
+    </ul>
+</div>
