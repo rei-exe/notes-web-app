eeb1c2ca97594a103faa6d58a972558d78cf4c11
Jamesnl55555 <jamesnl55555@gmail.com>
2024-10-07 18:56:26 +0800

    simplified the design

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<htm lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,13 +8,14 @@
     <link rel="stylesheet" href="">
 </head>
 <style>
+
     body{
         display: flex;
         justify-content: center;
         height: 100vh;
         margin: 0;
+        
     }
-
     header{
         display: flex;
         align-content: space-between;
@@ -27,28 +28,27 @@
     main{
         margin-bottom: auto;
     }
+    div{
+        border: thin;
+        border: solid;
+        text-align: center
+    }
+
 </style>
 <body>
-    <header>
-        <h1> Note Application</h1>
-        <nav>
-            <ul>
-                <li><a href="http://127.0.0.1:8000/home/about">About</a></li>
-                <li><a href="http://127.0.0.1:8000/home/contributors">Contributors</a></li>
-            </ul>
-        </nav>
-    </header>
 
-    <main>
-    <nav>
-        <ul>
-            <div class="container">
-                <li><a href="http://127.0.0.1:8000/notes">Create New Notes</a></li>
-                <li><a href="#"></a></li>
-                <li></li>
-            </div>
-        </ul>
-    </nav>
-    </main>
+        <h1> Note Application</h1>
+<div>
+    
+    <a href="http://127.0.0.1:8000/home/about">About</a><br>
+    <a href="http://127.0.0.1:8000/home/contributors">Contributors</a><br>
+    <a href="http://127.0.0.1:8000/notes">Create New Notes</a><br>
+</div>
+
+
+
+
+</form>
 </body>
+
 </html>
