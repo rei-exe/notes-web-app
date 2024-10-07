3b889db1c97e55e437a47b90bbb2b1162d49123a
Jamesnl55555 <jamesnl55555@gmail.com>
2024-10-07 14:55:21 +0800

    modified

--- app/Http/Controllers/CreateNotesController.php
+++ app/Http/Controllers/CreateNotesController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class CreateNotesController extends Controller
 {
-    //
+    public function create()
+    {
+        return view('create'); 
+    }
 }
