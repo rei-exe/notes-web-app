7296da4aada9d82add502c7867d30bb021a10299
Jamesnl55555 <jamesnl55555@gmail.com>
2024-10-07 15:02:10 +0800

    same, modified

--- app/Http/Controllers/EditNoteController.php
+++ app/Http/Controllers/EditNoteController.php
@@ -2,10 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
+use App\Models\Note;
 use Illuminate\Http\Request;
 
 class EditNoteController extends Controller
 {
-    //
+    public function edit($id)
+    {
+        $note = Note::findOrFail($id); # fetch for editing
+        return view('edit', compact('note')); # return edit view
+    }
 }
