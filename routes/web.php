08caaa045d277e37200cfe5f8d05ca0cdc1f2583
vinceaintreadin-21 <vincentleeduriga@student.laverdad.edu.ph>
2024-10-16 09:45:39 +0800

    Initialize

--- routes/web.php
+++ routes/web.php
@@ -7,10 +7,23 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/notes', [NoteController::class, 'showAllNotes']); 
+#showing all Notes
+Route::get('/notes', [NoteController::class, 'showAllNotes']);
+
+#creating notes
 Route::get('/notes/create', [NoteController::class, 'Create']);
-Route::post('/notes', [NoteController::class, 'store']); 
+
+#after creating notes it'll store it to the database
+Route::post('/notes', [NoteController::class, 'store']);
+
+#reading only one note
 Route::get('/notes/{id}', [NoteController::class, 'Read']);
+
+#updating notes through editing
 Route::get('/notes/{id}/edit', [NoteController::class, 'Edit']);
-Route::put('/notes/{id}', [NoteController::class, 'Update']); 
+
+#after done editing the data will be overrided
+Route::put('/notes/{id}', [NoteController::class, 'Update']);
+
+#this will delete the data from the database
 Route::delete('/notes/{id}', [NoteController::class, 'Delete']);
