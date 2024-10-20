13c02bc270c0aaa6bddca9e65d04d1844dbc9633
Jamesnl55555 <jamesnl55555@gmail.com>
2024-10-20 19:34:37 +0800

    modified

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -15,6 +15,10 @@
                     </button>
                 </form>
                 <h1>Create Xribe</h1>
+                <a class="Lmode" href="#" onclick="Lmode()"></a>
+                <a class="Dmode" href="#" onclick="Dmode()"></a>
+                <a class="Bmode" href="#" onclick="Bmode()"></a>
+                <a class="Ymode" href="#" onclick="Ymode()"></a>
             </nav>
         </header>
 
@@ -40,9 +44,30 @@
         </main>
 
         <script>
+
             function autoResize(textarea) {
                 textarea.style.height = 'auto'; // Reset height to allow shrinking
                 textarea.style.height = textarea.scrollHeight + 'px'; // Set height to scrollHeight
+            }
+            function Lmode(color){
+                document.querySelector("div").style.backgroundColor = "white";
+                document.querySelector("textarea").style.backgroundColor= "white";
+                document.querySelector("textarea").style.color= "black";
+            }
+            function Dmode(color){
+                document.querySelector("div").style.backgroundColor = "#222831";
+                document.querySelector("textarea").style.backgroundColor= "#1d1716";
+                document.querySelector("textarea").style.color= "green";
+            }
+            function Bmode(color){
+                document.querySelector("div").style.backgroundColor = "rgb(9, 49, 85)";
+                document.querySelector("textarea").style. backgroundColor= "#7fa99b";
+                document.querySelector("textarea").style.color= "black";
+            }
+            function Ymode(color){
+                document.querySelector("div").style.backgroundColor = "rgb(172, 170, 100)";
+                document.querySelector("textarea").style. backgroundColor= "#ffe79a";
+                document.querySelector("textarea").style.color= "black";
             }
         </script>
     </body>
