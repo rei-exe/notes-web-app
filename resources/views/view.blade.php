a68e727838c865a5cec09cec3d0363e498732a94
Jamesnl55555 <jamesnl55555@gmail.com>
2024-10-20 19:44:14 +0800

    modified

--- resources/views/view.blade.php
+++ resources/views/view.blade.php
@@ -14,6 +14,10 @@
                 </button>
             </form>
             <h1>Xribe</h1>
+            <a class="Lmode" href="#" onclick="Lmode()"></a>
+                <a class="Dmode" href="#" onclick="Dmode()"></a>
+                <a class="Bmode" href="#" onclick="Bmode()"></a>
+                <a class="Ymode" href="#" onclick="Ymode()"></a>
         </nav>
     </header>
 
@@ -24,5 +28,23 @@
             <div class="content"><p>{{$note->content}}</p></div>
         </div>
     </main>
+    <script>
+        function Lmode(color){
+        document.querySelector("div").style.backgroundColor = "white";
+        document.querySelector("div").style.color= "black";
+        }
+        function Dmode(color){
+        document.querySelector("div").style.backgroundColor = "#222831";
+        document.querySelector("div").style.color= "green";
+        }
+        function Bmode(color){
+        document.querySelector("div").style.backgroundColor = "rgb(9, 49, 85)";
+        document.querySelector("div").style.color= "black";
+        }
+        function Ymode(color){
+        document.querySelector("div").style.backgroundColor = "rgb(172, 170, 100)";
+        document.querySelector("div").style.color= "black";
+        }
+        </script>
 </body>
 </html>
